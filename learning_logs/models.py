a7a9c92a4a8ab9689f4bad19a6e96e9961cc8c23
0044000353 <li.shengsong@gd-linux.cn>
2022-08-17 11:39:17 +0800

    定义模型Entry

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
@@ -9,4 +10,20 @@ class Topic(models.Model):
     def __str__(self):
         # 返回模型的字符串表示
         return self.text
+
+
+class Entry(models.Model):
+    # 学到的有关某个主题的具体知识
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        # 返回模型的字符串表示
+        return f"{self.text[:50]}..."
+
+        
         
