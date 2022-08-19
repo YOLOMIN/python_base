cf3319518e1a7fec5670508c7759f4208a7ca668
0044000353 <li.shengsong@gd-linux.cn>
2022-08-19 10:30:40 +0800

    显示所有主题的页面

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from .models import Topic                               #导入所需数据相关联的模型
+
 # Create your views here.
 def index(request):
     # 学习笔记的主页
     return render(request,'learning_logs/index.html')
+
+def topics(request):                                    #Django从服务器哪里收到requst对象
+    # 显示所有的主题
+    topics = Topic.objects.order_by('date_added')       #查询数据库--请求Topic对象，并根据属性date_added进行排序
+    context = {'topics':topics}                         #定义一个将发送给模型的上下文
+    return render(request,'learning_logs/topics.html',context)
